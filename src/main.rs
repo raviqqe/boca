eb7e66ddb5354e5f2e727571544565215a56b345
Yota Toyama <raviqqe@gmail.com>
2025-07-10 19:46:08 -0700

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use cucumber::{World as _, given};
+use cucumber::{World as _, given, then, when};
+use std::error::Error;
 use tokio::process::Command;
 
 #[derive(Debug, Default, cucumber::World)]
@@ -7,9 +8,18 @@ struct World {
     capacity: usize,
 }
 
-#[given(expr = "I run \"{command}\"")]
-async fn run_command(_world: &mut World, command: String) {
-    Command::new(command).output().await;
+#[given(regex = "I run `(.*)`")]
+#[when(regex = "I run `(.*)`")]
+#[then(regex = "I run `(.*)`")]
+async fn run_command(_world: &mut World, command: String) -> Result<(), Box<dyn Error>> {
+    let command = command.split_whitespace().collect::<Vec<_>>();
+
+    Command::new(&command[0])
+        .args(&command[1..])
+        .output()
+        .await?;
+
+    Ok(())
 }
 
 #[tokio::main]
--- tests/command.feature
+++ tests/command.feature
@@ -1,17 +1,5 @@
 Feature: Command
   Scenario: Execute a command
-    Given I have a command "echo Hello, World!"
-    When I execute the command
-    Then the stdout should be "Hello, World!"
-
-  Scenario: Command with arguments
-    Given I have a command "echo"
-    And I have arguments "Hello, World!"
-    When I execute the command with arguments
-    Then the stdout should be "Hello, World!"
-
-  Scenario: Command with environment variables
-    Given I have a command "echo $GREETING"
-    And I set environment variable GREETING to "Hello, World!"
+    Given I run `echo hello world`
     When I execute the command
     Then the stdout should be "Hello, World!"

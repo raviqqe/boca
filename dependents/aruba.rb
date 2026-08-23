5df323de9d0b1e4b0c1aeeabf048ad64f4793910
Yota Toyama <raviqqe@gmail.com>
2026-08-23 15:19:57 -0700

    Fix

--- dependents/aruba.rb
+++ dependents/aruba.rb
@@ -1,1 +1,20 @@
 require 'aruba/cucumber'
+
+# Cucumber substitutes example values into scenario outlines as replacement templates, so a
+# backslash followed by a backtick in a value expands into the text before a match. Collapse
+# escaped backslashes, which feature files depend on, and substitute values literally instead.
+module Gherkin
+  module Pickles
+    class Compiler
+      def interpolate(name, variable_cells, value_cells)
+        variable_cells.each_with_index do |variable_cell, index|
+          value = value_cells[index].value.gsub('\\\\') { '\\' }
+
+          name = name.gsub("<#{variable_cell.value}>") { value }
+        end
+
+        name
+      end
+    end
+  end
+end
